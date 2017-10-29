9b84debb6851cb858631d5c3c8adb84cdbc70fa5
Ricky Cruz <rczdev@gmail.com>
2017-10-28 22:05:59 -0500

    Include favorited Replies in activity feed

--- app/Favorite.php
+++ app/Favorite.php
@@ -6,10 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Favorite extends Model
 {
+    use RecordsActivity;
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = ['user_id', 'favorited_id', 'favorited_type'];
+
+
+    /**
+     * Fetch the model that was favorited.
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\MorphTo
+     */
+    public function favorited()
+    {
+        return $this->morphTo();
+    }
 }
