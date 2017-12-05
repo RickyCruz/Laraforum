d7068730738ef3faeaff2d35d0de34300d1d525c
Ricky Cruz <rczdev@gmail.com>
2017-12-04 23:38:30 -0600

    Display the views count for each Thread using a class

--- app/Thread.php
+++ app/Thread.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Thread extends Model
 {
-    use RecordsActivity, RecordsVisits;
+    use RecordsActivity;
 
     /**
      * The attributes that are mass assignable.
@@ -171,5 +171,10 @@ class Thread extends Model
         $key = $user->visitedThreadCacheKey($this);
 
         return $this->updated_at > cache($key);
+    }
+
+    public function visits()
+    {
+        return new Visits($this);
     }
 }
--- resources/views/threads/_list.blade.php
+++ resources/views/threads/_list.blade.php
@@ -36,7 +36,7 @@
         </div>
 
         <div class="panel-footer">
-            {{ $thread->visits() }} Visits
+            {{ $thread->visits()->count() }} Visits
         </div>
     </div>
 @empty
