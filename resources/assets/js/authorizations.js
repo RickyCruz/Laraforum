f2c6a89806a508b474153966fccbe3ddc6e3ddfc
Ricky Cruz <rczdev@gmail.com>
2017-12-18 19:52:00 -0600

    Improve Thread authorization

--- resources/assets/js/authorizations.js
+++ resources/assets/js/authorizations.js
@@ -1,11 +1,11 @@
 let user = window.App.user;
 
 module.exports = {
-    updateReply(reply) {
-        return reply.user_id == user.id;
-    },
-
     updateAvatar(userProfile) {
         return userProfile.id == user.id;
+    },
+
+    owns(model, prop = 'user_id') {
+        return model[prop] == user.id;
     }
 };
--- resources/assets/js/components/Reply.vue
+++ resources/assets/js/components/Reply.vue
@@ -3,12 +3,12 @@
         <div class="panel-heading">
             <div class="level">
                 <h5 class="flex">
-                    <a :href="'/profiles/' + data.owner.name" v-text="data.owner.name">
+                    <a :href="'/profiles/' + reply.owner.name" v-text="reply.owner.name">
                     </a> said <span v-text="ago"></span>...
                 </h5>
 
                 <div v-if="signedIn">
-                    <favorite :reply="data"></favorite>
+                    <favorite :reply="reply"></favorite>
                 </div>
             </div>
         </div>
@@ -27,12 +27,12 @@
             <div v-else v-html="body"></div>
         </div>
 
-        <div class="panel-footer level">
-            <div v-if="authorize('updateReply', reply)">
+        <div class="panel-footer level" v-if="authorize('owns', reply) || authorize('owns', reply.thread)">
+            <div v-if="authorize('owns', reply)">
                 <button class="btn btn-xs btn-default mr-1" @click="editing = true">Edit</button>
                 <button class="btn btn-xs btn-danger mr-1" @click="destroy">Delete</button>
             </div>
-            <button class="btn btn-xs btn-info ml-a" @click="markBestReply" v-if="! isBest">Best Reply?</button>
+            <button class="btn btn-xs btn-info ml-a" @click="markBestReply" v-if=" authorize('owns', reply.thread)">Best Reply?</button>
         </div>
     </div>
 </template>
@@ -42,23 +42,22 @@
     import moment from 'moment';
 
     export default {
-        props: ['data'],
+        props: ['reply'],
 
         components: { Favorite },
 
         data() {
             return {
                 editing: false,
-                id: this.data.id,
-                body: this.data.body,
-                isBest: this.data.isBest,
-                reply: this.data
+                id: this.reply.id,
+                body: this.reply.body,
+                isBest: this.reply.isBest
             }
         },
 
         computed: {
             ago() {
-                return moment(this.data.created_at).fromNow();
+                return moment(this.reply.created_at).fromNow();
             }
         },
 
@@ -70,7 +69,7 @@
 
         methods: {
             update() {
-                axios.patch('/replies/' + this.data.id, {
+                axios.patch('/replies/' + this.id, {
                     body: this.body
                 })
                 .catch(error => {
@@ -83,15 +82,15 @@
             },
 
             destroy() {
-                axios.delete('/replies/' + this.data.id);
+                axios.delete('/replies/' + this.id);
 
-                this.$emit('deleted', this.data.id);
+                this.$emit('deleted', this.id);
             },
 
             markBestReply() {
-                axios.post('/replies/' + this.data.id + '/best');
+                axios.post('/replies/' + this.id + '/best');
 
-                window.events.$emit('best-reply-selected', this.data.id);
+                window.events.$emit('best-reply-selected', this.id);
             }
         }
     }
