5ef74cac4a7c8e5cd013c4565bae7e7ac18bf246
Ricky Cruz <rczdev@gmail.com>
2017-12-22 00:12:00 -0600

    An administrator may lock or unlock any Thread

--- app/Thread.php
+++ app/Thread.php
@@ -33,6 +33,15 @@ class Thread extends Model
     protected $appends = ['isSubscribedTo'];
 
     /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'locked' => 'boolean'
+    ];
+
+    /**
      * Boot the model.
      */
     protected static function boot()
@@ -103,14 +112,6 @@ class Thread extends Model
         event(new ThreadReceivedNewReply($reply));
 
         return $reply;
-    }
-
-    /**
-     * Lock the thread.
-     */
-    public function lock()
-    {
-        $this->update(['locked' => true]);
     }
 
     /**
