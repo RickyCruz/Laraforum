38d7c8fd290424eabaaee336f60e58b78821dd15
Ricky Cruz <rczdev@gmail.com>
2018-01-15 17:05:00 -0600

    Remove old stylesheet import for trix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,6 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/trix/0.11.1/trix.css" rel="stylesheet" >
 
     <!-- Scipts -->
     <script>
